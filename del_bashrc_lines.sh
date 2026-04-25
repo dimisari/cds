0e2727f6063d609298c50d6e59c2bf61d406c47d
dimisari <saridakisbitos@gmail.com>
2026-04-25 09:07:32 +0300

    del_bashrc_lines correct check for grep output

--- del_bashrc_lines.sh
+++ del_bashrc_lines.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-grep_out=$(grep -n $1 $2)
-if [ $? ]
+grep_out=$(grep -n bashrc_append $HOME/.bashrc)
+if [[ -n "$grep_out" ]]
 then
   line_num=$(echo $grep_out | cut -d ":" -f 1)
-  sed -i $(( $line_num-1 )),"$line_num"d $2
+  sed -i $(( $line_num-1 )),"$line_num"d $HOME/.bashrc
 fi
